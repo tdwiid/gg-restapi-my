bba4953fb3dca2c1c6ba7bf3841e8ecf4f21aeec
Dwi Cahya Sulistyawan <zhoelistca@hotmail.com>
2021-01-21 20:33:04 +0700

    Perubahan port definitif bagi program ini.

--- main.go
+++ main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/gin-gonic/gin"
-  "github.com/gg-restapi-my/controllers"
+	"github.com/gg-restapi-my/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  fmt.Println("hello world")
+	fmt.Println("hello world")
 
-  router := SetupRouter()
-  router.Run(":8081")
+	router := SetupRouter()
+	router.Run(":8077")
 }
 
-
 func SetupRouter() *gin.Engine {
-  router := gin.Default()
+	router := gin.Default()
 
-  v1 := router.Group("api/v1")
-  {
-    v1.POST("/flower", controllers.Create)
-    v1.GET("/flower/:id", controllers.GetFlower)
-    v1.GET("/flowers", controllers.GetAllFlower)
-    v1.DELETE("/flower", controllers.DeleteFlower)
-    v1.GET("/check", controllers.HealthCheck)
-  }
-  return router
+	v1 := router.Group("api/v1")
+	{
+		v1.POST("/flower", controllers.Create)
+		v1.GET("/flower/:id", controllers.GetFlower)
+		v1.GET("/flowers", controllers.GetAllFlower)
+		v1.DELETE("/flower", controllers.DeleteFlower)
+		v1.GET("/check", controllers.HealthCheck)
+	}
+	return router
 }
